a4414c94f50de8f8b13e12e5eb6e0bb4b9c9cab2
Fluk <rattanasak000@hotmail.com>
2018-03-14 15:00:12 +0100

    rattanasak: create actors and users table
    actors and Users

---
+++ group4-Movie/database/migrations/2018_03_14_135042_create_actor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('namn');
+              $table->string('info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
