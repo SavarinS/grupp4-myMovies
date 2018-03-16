12d8a62a75e162c02777460a64d7ea2f74a3eac9
Fluk <rattanasak000@hotmail.com>
2018-03-16 13:20:04 +0100

    add password and iforget in users

--- group4-Movie/database/migrations/2018_03_14_135305_create_users_table.php
+++ group4-Movie/database/migrations/2018_03_14_135305_create_users_table.php
@@ -16,7 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
               $table->string('namn');
-                $table->string('email');
+                $table->string('email')->unique();
+                  $table->string('password');
+                  $table->rememberTorken();
             $table->timestamps();
         });
     }
