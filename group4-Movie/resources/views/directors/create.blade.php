7c2b7a3c0a3b5d54945ccc3cc41ac8a98c978d12
Savarin Sitisarn <savarin.sitisarn@gmail.com>
2018-03-16 15:37:14 +0100

    Director page

---
+++ group4-Movie/resources/views/directors/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts/app')
+@section('content')
+<h3>Create new director</h3>
+<form method="POST" action="{{ route('directors.store') }}">
+
+    {{ csrf_field() }}
+
+    <div class="form-group">
+
+        <input type="text" class="form-control" placeholder="Director's name" name="name" required><br>
+        <!-- <textarea class="form-control" name="movie_id" placeholder="Fill in director's work" required></textarea><br> -->
+
+        <textarea class="form-control" name="info" placeholder="Fill in director's biography" required></textarea><br>
+
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</form>
+
+<!-- show alert messages when users don't fill in the form -->
+<!--@if(count($errors))
+    <div class="alert alert-primary" role="alert">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach       
+        </ul>
+    </div>
+    @endif-->
+@endsection
--- group4-Movie/resources/views/directors/index.blade.php
+++ group4-Movie/resources/views/directors/index.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts/page')
+@extends('layouts/app')
 
-@section('page')
+@section('content')
 <h3>My favourit directors</h3>
 <ul>
     @foreach ($directors as $director)
@@ -8,6 +8,7 @@
     @endforeach
 </ul>
 
+<button type="button" class="btn btn-outline-primary"><a href="{{route('directors.create')}} ">Create new director</button><br>
 <a href="{{ route ('home') }}"> Back to Home</a>
 @endsection
 
