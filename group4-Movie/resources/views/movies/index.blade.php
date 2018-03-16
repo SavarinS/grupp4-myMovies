ece599d8873dc5c0784c8a405f640823a7a78025
Natta <nattarintra.thutsanti@hotmail.com>
2018-03-16 22:05:48 +0100

    Natta: Edited Movies index,
    Remember? We edtited @extends('layouts/page') To @extends('layouts/app')  This is the right file We are going to use.

--- group4-Movie/resources/views/movies/index.blade.php
+++ group4-Movie/resources/views/movies/index.blade.php
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-@extends('layouts/page')
 
-<?php
-
- ?>
-
-@section('movie-index')
-
-<h2>My favourite movies</h2>
-@foreach($movies as $movie)
-
-<img src="{{$movie->coverphoto}}"><br>
-<h4>Movie Title : {{$movie->title}}</h4> 
-<p>Description : {{$movie->desc}}</p>  
-<p>Run Times :{{$movie->runtimes}}</p>  
-<p>Release Year : {{$movie->releaseyear}} </p>
-
-
-@endforeach
-
-<div>
-<button type="button" class="btn btn-outline-primary"><a href="{{route('movies.create')}} ">Create new movies</button>
-  
-</div>
-
-@endsection
-=======
 @extends('layouts/app')
 
 @section('content')
@@ -33,9 +6,9 @@
   @foreach($movies as $movie)
 
     <img src="{{$movie->coverphoto}}" alt=""> <br>
-    <h4>Movie Title : {{$movie->title}}</h4>
+    <h4>Movie Title : <a href="{{route('movies.show', ['movie'=>$movie->id])}}"> {{$movie->title}}</a></h4>
     <p>Description : {{$movie->desc}}</p>
-    <p>Run Times :{{$movie->runtimes}}</p>
+    <p>Run Times :{{$movie->runtimes}} mins</p>
     <p>Release Year : {{$movie->releaseyear}} </p>
   @endforeach
 
@@ -43,4 +16,4 @@
       <button type="button" class="btn btn-outline-primary"><a href="{{route('movies.create')}} ">Create new movies</button>
   </div>
 @endsection
->>>>>>> master
+
